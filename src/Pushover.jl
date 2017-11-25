82db6f89cab9005df3629012bce66e43a08db6ba
scls19fr <s.celles@gmail.com>
2017-11-25 09:38:26 +0100

    docstring

--- src/Pushover.jl
+++ src/Pushover.jl
@@ -1,3 +1,7 @@
+"""
+A Julia package to send notifications using the [Pushover Notification Service](https://pushover.net/) 
+as documented [here](https://pushover.net/api).
+"""
 module Pushover
 
     export PushoverClient, send
@@ -10,11 +14,24 @@ module Pushover
     _DEFAULT_MAX_TITLE_LEN = 100
     _DEFAULT_MAX_MESSAGE_LEN = 512
 
+    """
+        PushoverException(response)
 
+    Pushover exception
+    """
     struct PushoverException <: Exception
         response::Dict
     end
 
+    """
+        PushoverClient(user_key, api_token; max_title_len=_DEFAULT_MAX_TITLE_LEN, max_message_len=_DEFAULT_MAX_MESSAGE_LEN)
+
+    Create a client that can connect to Pushover Notification Service.
+
+    - `user_key` and `api_token` are required.
+    - `max_title_len` is optional and is set by default to `_DEFAULT_MAX_TITLE_LEN = 100`.
+    - `max_message_len` is optional and is set by default to `_DEFAULT_MAX_MESSAGE_LEN = 512`
+    """
     struct PushoverClient
         user_key::AbstractString
         api_token::AbstractString
@@ -29,6 +46,12 @@ module Pushover
         end
     end
 
+    """
+        _sanitize_priority(priority) -> sanitized_priority
+
+    Sanitize `priority` (must be an integer in [-2;1] range).
+    If `priority` is not in the correct range, `0` is returned.
+    """
     function _sanitize_priority(priority)
         if !in(priority, [-2, -1, 0, 1])
             priority = 0
@@ -36,6 +59,11 @@ module Pushover
         priority
     end
 
+    """
+        _crop(msg, max_len) -> cropped_msg
+
+    Crop a `message` to `max_len`.
+    """
     function _crop(msg, max_len)
         if max_len > 0 && length(msg) > max_len
             msg[1:max_len-3] * "..."
@@ -44,7 +72,19 @@ module Pushover
         end
     end
 
+    """
+        send(client::PushoverClient, message; device=nothing, title=nothing, url=nothing, url_title=nothing, priority=nothing, timestamp=nothing, sound=nothing)
 
+    Send a `message` to Pushover notification service using `client`.
+    The following parameters are optional.
+    - `device`
+    - `title`
+    - `url`
+    - `url_title`
+    - `priority`
+    - `timestamp`
+    - `sound`
+    """
     function send(client::PushoverClient, message::AbstractString;
                 device=nothing, title=nothing, url=nothing, url_title=nothing,
                 priority=nothing, timestamp=nothing, sound=nothing)
