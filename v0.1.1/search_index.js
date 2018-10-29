58e6e8350116f6a6c57d8437b054b5e9726e6bd9
autodocs <autodocs>
2018-10-29 11:01:50 +0000

    build based on 02a6480

---
+++ v0.1.1/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Pushover.jl Overview",
+    "title": "Pushover.jl Overview",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Pushover",
+    "page": "Pushover.jl Overview",
+    "title": "Pushover",
+    "category": "module",
+    "text": "A Julia package to send notifications using the Pushover Notification Service  as documented here.\n\n\n\n\n\n"
+},
+
+{
+    "location": "#Pushover.jl-Overview-1",
+    "page": "Pushover.jl Overview",
+    "title": "Pushover.jl Overview",
+    "category": "section",
+    "text": "Pushover"
+},
+
+{
+    "location": "#Install-1",
+    "page": "Pushover.jl Overview",
+    "title": "Install",
+    "category": "section",
+    "text": "Pkg.add(\"Pushover\")orPkg.clone(\"https://github.com/scls19fr/Pushover.jl\")"
+},
+
+{
+    "location": "#Usage-1",
+    "page": "Pushover.jl Overview",
+    "title": "Usage",
+    "category": "section",
+    "text": "using Markdown\nMarkdown.parse(\"\"\"\n```\n$(read(\"../../sample/send_message.jl\", String))\n```\n\"\"\")See examples"
+},
+
+{
+    "location": "#See-also-1",
+    "page": "Pushover.jl Overview",
+    "title": "See also",
+    "category": "section",
+    "text": "BulkSMS.jl - A Julia package to send SMS (Short Message Service) using BulkSMS API"
+},
+
+]}
